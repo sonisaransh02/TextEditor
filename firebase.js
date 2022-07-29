478d54a5118b57057c8d42b64145cc54b2141cd8
sonisaransh02 <60021305+sonisaransh02@users.noreply.github.com>
2022-07-29 16:03:54 +0530

    remove apikey info before commiting changes, fool

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import { FirebaseAdapter } from '@next-auth/firebase-adapter';
 import firebase from 'firebase';
 const firebaseConfig = {
-    apiKey: "AIzaSyC6p8A0KgBVcfyO7G4GsOU2E_qtdbzirHk",
-    authDomain: "text-editor-69.firebaseapp.com",
-    projectId: "text-editor-69",
-    storageBucket: "text-editor-69.appspot.com",
-    messagingSenderId: "351009020618",
-    appId: "1:351009020618:web:2f5c5ebf18209693717233"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const app = !firebase.apps.length 
